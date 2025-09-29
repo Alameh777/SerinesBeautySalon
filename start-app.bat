053d3b23365797ce2dbf9e4220abf8c51f504eb0
Alameh777 <alamehmhmd5@gmail.com>
2025-09-29 23:39:49 +0300

    ADDED THE APP THING

--- start-app.bat
+++ start-app.bat
@@ -1,21 +1,24 @@
 @echo off
-echo Starting Laravel Application...
-echo.
+echo Starting Apache and MySQL...
 
-REM Start Apache and MySQL
-echo Starting XAMPP services...
+REM Start Apache
 cd /d "C:\xampp"
-start "" "C:\xampp\xampp-control.exe"
+start /B apache\bin\httpd.exe
 
-REM Wait a few seconds for services to start
+REM Start MySQL
+start /B mysql\bin\mysqld.exe --defaults-file=mysql\bin\my.ini
+
+REM Wait for services to initialize
 timeout /t 5 /nobreak > nul
 
-REM Start the Laravel application in browser
-echo Opening application in browser...
+REM Open browser
 start "" "http://localhost/BeautySalon/public"
 
 echo.
-echo Application is running!
-echo To stop: Close this window and stop XAMPP services
-echo.
+echo Services are running in background
+echo Close this window to stop them
 pause
+
+REM Stop services when user closes
+taskkill /F /IM httpd.exe
+taskkill /F /IM mysqld.exe
